5af9d8d877af542d7604e01caa6e5f5e98b7f8b8
ToniNichev <toni.nichev@gmail.com>
2022-02-24 11:34:26 -0500

    #restructuring

--- src/thermostats/string_to_float.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRING_TO_FLOAT_H
-#define STRING_TO_FLOAT_H
-#include <Arduino.h>
-
-
-float *parseToValues(String serverBodyResponse);
-String valuesToString(double valueAray[], short int count);
-#endif
